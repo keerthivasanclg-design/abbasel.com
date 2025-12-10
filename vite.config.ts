e06b947c82a2106c706bca04a24bd08f45cd396b
keerthivasan <keerthivasanclgdesign@gmail.com>
2025-12-10 14:22:23 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,20 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+// vite.config.ts
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-
+    base: '/abbasel.com/', // <- match your GitHub repo name exactly
     plugins: [
       react(),
       tailwindcss()
     ],
-
-    base: '/AbbaselDigitalMarketing/',
-
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
-
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
